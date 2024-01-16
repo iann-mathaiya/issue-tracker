1151f44a3d4ec399b84dafc58590e0e120392e2d
Ian Mathaiya <iannmathaiya@gmail.com>
2024-01-16 11:27:56 +0300

    added issues page ui and added typography components

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,16 @@
+import { TypographyH1 } from "@/components/typography/typography-h1"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
 export default function Issues() {
   return (
-    <div>Issues Page</div>
+    <div className='max-w-7xl mx-auto py-4 sm:py-8'>
+      <div className="flex items-center justify-between">
+        <TypographyH1>Issues</TypographyH1>
+        <Button variant='link'>
+          <Link href='/issues/new-issue'>Create New Issue +</Link>
+        </Button>
+      </div>
+    </div>
   )
 }
